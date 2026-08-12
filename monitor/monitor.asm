5d210a3a4dff051f9b73b5f71c4a08f9848022fe
Litvin Vasil <purrrock@gmail.com>
2026-08-12 21:30:06 +0300

    Monitor update

--- monitor/monitor.asm
+++ monitor/monitor.asm
@@ -1,24 +1,24 @@
 ORG 0x00
 ENTRY:
-    ; Единая точка входа
-    LDI 0x8         
-    MOV B, A
-    MOV A, FL       
-    AND B           
-    JZ CMD_LOOP     ; Возврат из SWI (R=0)
-    
-    LDI 0x4         
-    MOV FL, A       ; Холодный старт (R=1, M=1)
-
-CMD_LOOP:
+    ; Unified entry after RESET or SWI
+    LDI 0x8
+    MOV B,A
+    MOV A,FL
+    AND B
+    JZ MON_INIT
+    ; Cold start: clear R, keep M=1
+    LDI 0x4
+    MOV FL,A
+MON_INIT:
+    ; Reset display mode and restore MMIO pointer
     LDI 0xF
-    MOV X, A
+    MOV X,A
     LDI 0x0
-    MOV Y, A
-    STR             ; DISP_0 = 0x0
-
+    MOV Y,A
+    STR
 WAIT_CMD:
-    CAL GETKEY      
+    ; Read command: A / B
+    CAL GETKEY
     LDI 0xA
     SUB B
     JZ LOAD
@@ -26,140 +26,127 @@ WAIT_CMD:
     SUB B
     JZ RUN
     JMP WAIT_CMD
-
 LOAD:
+    ; DISP_0 = A, current address = 0x10
     LDI 0x0
-    MOV Y, A
+    MOV Y,A
     LDI 0xA
-    STR             ; DISP_0 = 0xA
-    
-    CAL GETKEY      ; Возвращает код в A и B. Y остается равным 4 после GETKEY
-    DEC Y           ; Y = 3 (Указатель на DISP_3)
-    STR             ; DISP_3 = PCH
-    
-    CAL GETKEY      
-    LDI 0x2
-    MOV Y, A
-    STR             ; DISP_2 = PCL
-
-LOAD_LOOP:
-    CAL SET_PTR     ; X=PCH, Y=PCL, A=RAM[X:Y]
-    MOV B, A        ; B = RAM[X:Y]
-    LDI 0xF
-    MOV X, A
+    STR
+    LDI 0x3
+    MOV Y,A
     LDI 0x1
-    MOV Y, A
-    MOV A, B
-    STR             ; DISP_1 = RAM[X:Y] (Динамическая индикация)
-    
-    CAL GETKEY      
+    STR
+    LDI 0x2
+    MOV Y,A
+    LDI 0x0
+    STR
+LOAD_LOOP:
+    ; DISP_1 = current RAM value
+    CAL SET_PTR
+    LDI 0x1
+    MOV Y,A
+    STR
+    ; Read next input nibble
+    CAL GETKEY
     LDI 0xF
     SUB B
     JZ ESCAPE
-
 WRITE_NIBBLE:
-    CAL SET_PTR     
-    MOV A, B        ; Восстановление кода клавиши в A
-    STR             ; RAM[X:Y] = A
-    CAL ADVANCE
+    ; Write B to current RAM address
+    CAL SET_PTR
+    MOV A,B
+    STR
+    ; Increment low address nibble
+    LDI 0xF
+    MOV X,A
+    LDI 0x2
+    MOV Y,A
+    LDR
+    INC A
+    STR
+    JC ADV_HIGH
     JMP LOAD_LOOP
-
 ESCAPE:
+    ; F0 = end of LOAD, FF = literal F
     CAL GETKEY
     LDI 0x0
     SUB B
-    JZ CMD_LOOP     ; F0 -> Выход
-    
+    JZ MON_INIT
     LDI 0xF
-    MOV B, A        ; FF -> Литерал F
+    SUB B
+    JZ LITERAL_F
+    JMP MON_INIT
+LITERAL_F:
+    LDI 0xF
+    MOV B,A
     JMP WRITE_NIBBLE
-
 RUN:
+    ; Run fixed COM entry point 0x10
     LDI 0x0
-    MOV Y, A
-    LDI 0xB
-    STR             ; DISP_0 = 0xB
-    
-    CAL GETKEY
-    MOV PCH, A      ; Прямая загрузка из A
-    
-    CAL GETKEY
+    MOV Y,A
+    MOV A,B
+    STR
+    LDI 0x1
+    MOV PCH,A
     LDI 0x0
-    MOV FL, A       ; M = 0 (User Mode)
-    MOV A, B        ; Восстановление кода клавиши
-    MOV PCL, A      ; Запуск пользовательского кода
-
+    MOV FL,A
+    MOV PCL,A
 SET_PTR:
-    LDI 0xF
-    MOV X, A
+    ; X=0xF on entry; DISP_3:DISP_2 = current address
     LDI 0x3
-    MOV Y, A
+    MOV Y,A
     LDR
-    MOV PCH, A      ; Использование PCH как регистра-скратчпада
-    DEC Y           ; Y = 2 (DISP_2)
+    MOV PCH,A
+    DEC Y
     LDR
-    MOV Y, A        ; Y = PCL
-    MOV A, PCH
-    MOV X, A        ; X = PCH
-    LDRA            ; Чтение RAM[X:Y]
+    MOV Y,A
+    MOV A,PCH
+    MOV X,A
+    LDRA
     RET
-
-ADVANCE:
-    ; Инкремент младшего полубайта адреса (DISP_2)
-    LDI 0xF
-    MOV X, A
-    LDI 0x2
-    MOV Y, A
-    LDR
-    MOV B, A
-    INC B           ; AЛУ: B = B + 1. Аппаратно сбрасывает C=0 или устанавливает C=1 при 0xF->0x0
-    MOV A, B        ; Переносим результат обратно в A для записи
-    STR             
-    JC ADV_HIGH     ; Чистый переход при переполнении страницы памяти
-    RET
-
 ADV_HIGH:
-    ; Инкремент старшего полубайта адреса (DISP_3)
+    ; Increment high nibble; E0 means end of COM area
     LDI 0x3
-    MOV Y, A
+    MOV Y,A
     LDR
-    MOV B, A
-    INC B           ; AЛУ: B = B + 1
-    MOV A, B
-    STR             ; DISP_3 = DISP_3 + 1
-    RET
-
+    INC A
+    STR
+    MOV B,A
+    LDI 0xE
+    SUB B
+    JZ MON_INIT
+    JMP LOAD_LOOP
 GETKEY:
-    ; Инициализация указателя на порт GPI (0xF4)
+    ; X:Y = GPI_KBD, B = status mask
     LDI 0xF
-    MOV X, A
+    MOV X,A
     LDI 0x4
-    MOV Y, A
+    MOV Y,A
     LDI 0x1
-    MOV B, A        ; Резервируем B как маску (0x1) для АЛУ
-    
-K_WAIT_P:
-    LDR             ; Чтение состояния порта в аккумулятор A. Флаги НЕ меняются!
-    AND B           ; Пропускаем через АЛУ: A = A & 1. АЛУ обновляет флаг Z!
-    JZ K_WAIT_P     ; Теперь Z отражает реальное состояние пина. Если 0 -> ждем.   
-    
+    MOV B,A
+WAIT_PRESS:
+    ; LDR preserves Z, so AND must update it
+    LDR
+    AND B
+    JZ WAIT_PRESS
+    ; Read latched key code
     LDI 0x5
-    MOV Y, A
+    MOV Y,A
     LDR
-    MOV PCH, A      ; Сохранение кода клавиши
-    
+    MOV PCH,A
+    ; Dynamic key overlay on DISP_1
     LDI 0x1
-    MOV Y, A
-    STR             ; DISP_1 = Введенный ниббл
-    
+    MOV Y,A
+    MOV A,PCH
+    STR
+    ; Wait until key is released
     LDI 0x4
-    MOV Y, A
-    
-K_WAIT_R:
+    MOV Y,A
+WAIT_RELEASE:
     LDR
-    SUB B           ; Если кнопка нажата, A=1. 1 - 1 = 0 (Z=1)
-    JZ K_WAIT_R     
-    
-    MOV A, PCH      ; Восстановление кода в A
-    MOV B, A        ; Дублирование кода в B для ABI
+    SUB B
+    JZ WAIT_RELEASE
+    ; Return code in both A and B
+    MOV A,PCH
+    MOV B,A
     RET
